c788ba3596e297a737f00913c9a03214c6540da9
Manpreet1007 <manpreetgandhi01@gmail.com>
2024-12-01 09:49:15 +0530

    Q2.dart

--- dart conditional and switch assignment/dart assignment conditionals/Q2.dart
+++ dart conditional and switch assignment/dart assignment conditionals/Q2.dart
@@ -1,7 +1,7 @@
 void main() {
-  int x = 5;
-  int y = 15;
-  int z = 10;
+  int x = 2;
+  int y = 4;
+  int z = 8;
   if (x > y && x > z) {
     print('Maximum of $x, $y, and $z is $x');
   } else if (y > z) {
